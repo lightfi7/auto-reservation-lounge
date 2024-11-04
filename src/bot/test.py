fb13cba61f5cbf8e9739446e4e30eec0c6059d14
Fi <158217226+lightfi7@users.noreply.github.com>
2024-11-04 09:46:12 -0800

    fixed

--- src/bot/test.py
+++ src/bot/test.py
@@ -46,8 +46,8 @@ def award(request, emulator, task):
 
 
     # Start
-    driver = webdriver.Remote(emulator["server_url"], options=UiAutomator2Options().load_capabilities(capabilities))
-    wait = WebDriverWait(driver, 30)
+    # driver = webdriver.Remote(emulator["server_url"], options=UiAutomator2Options().load_capabilities(capabilities))
+    # wait = WebDriverWait(driver, 30)
     # el = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
     # el.click()
 
@@ -65,84 +65,84 @@ def award(request, emulator, task):
 
     # Business Lounge \ Travel
     try:
-        el_business = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
-                                                             '//android.widget.ImageView[@content-desc="Выбрать\nбизнес-зал\nПутешествуйте\nс комфортом"]')))
-        el_business.click()
-
+        # el_business = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
+        #                                                      '//android.widget.ImageView[@content-desc="Выбрать\nбизнес-зал\nПутешествуйте\nс комфортом"]')))
+        # el_business.click()
         #
-        el_search = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.EditText')))
-        el_search.click()
-        title, location = action['Actions'][0]['Find'].split('@')
-        el_search.send_keys(title)
-
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, f'//android.view.View//*[contains(@content-desc, "{location}")]')))
-
-        print(el.get_attribute('content-desc'))
-        el.click()
-
-        el = None
-        name, group = action['Actions'][1]['Find'].split('@')
-        while True:
-            print('=>')
-            el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, f'//android.view.View//*[contains(@content-desc, "{name}\n{group}")]')))
-            if el is None:
-                driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-                                    'new UiScrollable(new UiSelector().scrollable(true)).flingToEnd(5)')
-            else:
-                print(';)')
-                break
-            time.sleep(1)
-            if is_at_bottom(driver):
-                print(';(')
-                break
-        el.click()
-
-        # el = wait.until(EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, 'Авторизоваться')))
-        # if el is None:
-        #     print('Авторизоваться')
-        #     return
+        # #
+        # el_search = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.EditText')))
+        # el_search.click()
+        # title, location = action['Actions'][0]['Find'].split('@')
+        # el_search.send_keys(title)
+        #
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, f'//android.view.View//*[contains(@content-desc, "{location}")]')))
+        #
+        # print(el.get_attribute('content-desc'))
         # el.click()
-
-        el = None
-        while True:
-            driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-                                    'new UiScrollable(new UiSelector().scrollable(true)).flingToEnd(5)')
-            time.sleep(1)
-            if is_at_bottom(driver):
-                break
-
-        first_name, last_name = action['Actions'][2]['Name'].split('@')
-
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.EditText[1]/android.widget.ImageView')))
-        el.click()
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
-                                                    '//android.widget.ScrollView/android.view.View/android.widget.EditText[1]')))
-        el.click()
-        el.send_keys(first_name)
-
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.EditText[2]/android.widget.ImageView')))
-        el.click()
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
-                                                    '//android.widget.ScrollView/android.view.View/android.widget.EditText[2]')))
-        el.click()
-        el.send_keys(last_name)
-
-        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.ImageView[3]')))
+        #
+        # el = None
+        # name, group = action['Actions'][1]['Find'].split('@')
+        # while True:
+        #     print('=>')
+        #     el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, f'//android.view.View//*[contains(@content-desc, "{name}\n{group}")]')))
+        #     if el is None:
+        #         driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+        #                             'new UiScrollable(new UiSelector().scrollable(true)).flingToEnd(5)')
+        #     else:
+        #         print(';)')
+        #         break
+        #     time.sleep(1)
+        #     if is_at_bottom(driver):
+        #         print(';(')
+        #         break
         # el.click()
-
-        return
-
-        el = wait.until(EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, 'Оформить')))
-        el.click()
-
-        # get qr code
-        base64_image = driver.get_screenshot_as_png()
+        #
+        # # el = wait.until(EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, 'Авторизоваться')))
+        # # if el is None:
+        # #     print('Авторизоваться')
+        # #     return
+        # # el.click()
+        #
+        # el = None
+        # while True:
+        #     driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+        #                             'new UiScrollable(new UiSelector().scrollable(true)).flingToEnd(5)')
+        #     time.sleep(1)
+        #     if is_at_bottom(driver):
+        #         break
+        #
+        # first_name, last_name = action['Actions'][2]['Name'].split('@')
+        #
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.EditText[1]/android.widget.ImageView')))
+        # el.click()
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
+        #                                             '//android.widget.ScrollView/android.view.View/android.widget.EditText[1]')))
+        # el.click()
+        # el.send_keys(first_name)
+        #
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.EditText[2]/android.widget.ImageView')))
+        # el.click()
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH,
+        #                                             '//android.widget.ScrollView/android.view.View/android.widget.EditText[2]')))
+        # el.click()
+        # el.send_keys(last_name)
+        #
+        # # el = wait.until(EC.element_to_be_clickable((AppiumBy.XPATH, '//android.widget.ScrollView/android.view.View/android.widget.ImageView[3]')))
+        # # el.click()
+        #
+        # return
+        #
+        # el = wait.until(EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, 'Оформить')))
+        # el.click()
+        #
+        # # get qr code
+        # base64_image = driver.get_screenshot_as_png()
         #
 
         request.app.database["tasks"].update_one({"_id": emulator["_id"]}, {"$set": {
             "success": True,
             "log": "Success",
-            "base64_image": f"{base64_image}"
+            "base64_image": f"{'base64_image'}"
         }})
         # driver.back()
         # driver.back()
